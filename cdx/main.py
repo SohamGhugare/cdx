969fe3d0802ed1b59722ef04487397357682f1ec
SohamGhugare <soham.ghugare@gmail.com>
2023-02-07 16:50:28 +0530

    Added callback function

--- cdx/main.py
+++ cdx/main.py
@@ -3,6 +3,10 @@ from rich import print
 
 app = typer.Typer()
 
+@app.callback()
+def callback():
+    """ CDX - A powerful CLI application for aliasing your project paths """
+
 @app.command(
     name="init",
     help="Initialize a cdx config"
